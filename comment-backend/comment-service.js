80adc816269fbef2e823d026ae1adeca93f398a2
Rajat <rajatpathak786@gmail.com>
2024-02-15 17:15:47 +0530

    Fixed logic to add comment to an existing post

--- comment-backend/comment-service.js
+++ comment-backend/comment-service.js
@@ -1,7 +1,9 @@
 const comments = [];
 
 const getAllCommentsByPostId = (req) => {
-  const commentsByPostId = comments.find((o) => o.hasOwnProperty(req.param.id));
+  const commentsByPostId = comments.find((o) =>
+    o.hasOwnProperty(req.params.id)
+  );
   return commentsByPostId;
 };
 
@@ -9,12 +11,16 @@ const addComment = (req) => {
   const comment = {
     content: req.body.content,
   };
-  const commentsByPostId = comments.find((o) => o.hasOwnProperty(req.param.id));
-  if (commentsByPostId.length) {
-    commentsByPostId.push(comment);
+  const commentsByPostId = comments.find((o) => {
+    if (o.hasOwnProperty(req.params.id)) {
+      o[req.params.id].push(comment);
+      return o;
+    }
+  });
+  if (commentsByPostId) {
     return comment;
   }
-  comments.push({ [req.id]: [...comment] });
+  comments.push({ [req.params.id]: [comment] });
   return comment;
 };
 
